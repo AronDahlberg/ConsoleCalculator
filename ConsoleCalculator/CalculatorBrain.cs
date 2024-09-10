2fe0ce0526d825404d8ed6716f4913a242723557
Aron Dahlberg <aron.t.dahlberg@gmail.com>
2024-09-10 15:58:39 +0200

    Improved the CalculatorBrainClass

--- ConsoleCalculator/CalculatorBrain.cs
+++ ConsoleCalculator/CalculatorBrain.cs
@@ -2,27 +2,42 @@
 {
     internal class CalculatorBrain
     {
-        public CalculatorBrain()
+        public double? Calculate(int function, double input1, double input2)
         {
+            double? output;
 
+            switch(function)
+            {
+                case 1: output = Addition(input1, input2); break;
+
+                case 2: output = Subtraction(input1, input2); break;
+
+                case 3: output = Multiplication(input1, input2); break;
+
+                case 4: output = Division(input1, input2); break;
+
+                default: output = null; break;
+            }
+
+            return output; 
         }
 
-        public int Addition(int value1, int value2)
+        public double Addition(double value1, double value2)
         {
             return value1 + value2;
         } 
 
-        public int Subtraction(int value1, int value2)
+        public double Subtraction(double value1, double value2)
         {
             return value1 - value2;
         }
 
-        public int Multiplication(int value1, int value2)
+        public double Multiplication(double value1, double value2)
         {
             return value1 * value2;
         }
 
-        public double Division(int value1, int value2)
+        public double Division(double value1, double value2)
         {
             return value1 / value2;
         }
